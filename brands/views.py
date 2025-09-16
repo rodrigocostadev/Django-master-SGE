a471ba407618117cf48c68d0a78974989617fd72
Rodrigo Costa <rodrigo.costa94@yahoo.com.br>
2025-09-15 22:27:38 -0300

    aula 27 concluida

--- brands/views.py
+++ brands/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
-from django.views.generic import ListView
-from . import models
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView
+from . import models, forms
 
 class BrandListView(ListView):
     model = models.Brand
@@ -16,3 +17,16 @@ class BrandListView(ListView):
             queryset = queryset.filter(name__icontains=name)
 
         return queryset
+    
+
+class BrandCreateView(CreateView):
+    model = models.Brand
+    template_name = 'brand_create.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+
+class BrandDetailView(DetailView):
+    model = models.Brand
+    template_name = 'brand_detail.html'
+
