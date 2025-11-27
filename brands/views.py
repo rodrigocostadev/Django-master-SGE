ecdf389d3c292cd87ef6ce9a24ef0240b10d3881
Rodrigo Costa <rodrigo.costa94@yahoo.com.br>
2025-11-26 22:35:44 -0300

    aula 30 concluida

--- brands/views.py
+++ brands/views.py
@@ -1,12 +1,13 @@
 # from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from . import models, forms
 
 class BrandListView(ListView):
     model = models.Brand
     template_name = 'brand_list.html'
     context_object_name = 'brands'
+    paginate_by = 10
 
     # Filtra o nome da marca no campo de busca da pagina
     def get_queryset(self): # (sobrescrevendo a função queryset que antes pegava todos os campos da model Brand)
@@ -29,4 +30,16 @@ class BrandCreateView(CreateView):
 class BrandDetailView(DetailView):
     model = models.Brand
     template_name = 'brand_detail.html'
+
+class BrandUpdateView(UpdateView):
+    model = models.Brand
+    template_name = 'brand_update.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+class BrandDeleteView(DeleteView):
+    model = models.Brand
+    template_name = 'brand_delete.html'
+    success_url = reverse_lazy('brand_list')
+
 
