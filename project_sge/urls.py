70e52aa4090103bf9cd85e46a656407962177023
Rodrigo Costa <rodrigo.costa94@yahoo.com.br>
2025-11-28 17:33:23 -0300

    aula 32 concluida

--- project_sge/urls.py
+++ project_sge/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('brands.urls')),
-    # path('', include('categories.urls')),
+    path('', include('categories.urls')),
     # path('', include('inflows.urls')),
     # path('', include('outflows.urls')),
     # path('', include('products.urls')),
-    # path('', include('suppliers.urls')),
+    path('', include('suppliers.urls')),
 ]
