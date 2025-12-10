5977af5f95f2e5363904a562f75c177058506117
Rodrigo Costa <rodrigo.costa94@yahoo.com.br>
2025-12-09 22:32:06 -0300

    aula 53 concluida (criando rota de login)

--- project_sge/metrics.py
+++ project_sge/metrics.py
@@ -1,7 +1,10 @@
 from products.models import Product
 from django.utils.formats import number_format
+from django.utils import timezone
 from outflows.models import Outflow
-from django.db.models import Sum
+from categories.models import Category
+from brands.models import Brand
+from django.db.models import Sum, F
 
 # Retorna as metricas de produto para mostrar no dashboard
 def get_product_metrics():
@@ -34,4 +37,51 @@ def get_sales_metrics():
         total_sales_value=number_format(total_sales_value, decimal_pos=2, force_grouping=True),
         total_sales_profit=number_format(total_sales_profit, decimal_pos=2, force_grouping=True),
     )
+
+
+def get_daily_sales_data():
+    today = timezone.now().date()
+    dates = [str(today - timezone.timedelta(days=i)) for i in range(6, -1, -1)] # lista de datas em string dos ultimos 7 dias, ex: 07/01/24, 08/01/24  ...
+    values = list()
+
+    for date in dates:
+        sales_total = Outflow.objects.filter(
+            created_at__date=date
+        ).aggregate(
+            total_sales=Sum(F('product__selling_price') * F('quantity'))
+        )['total_sales'] or 0
+        values.append(float(sales_total))
+
+    return dict(
+        dates=dates,
+        values=values,
+    )
+
+
+def get_daily_sales_quantity_data():
+    today = timezone.now().date()
+    dates = [str(today - timezone.timedelta(days=i)) for i in range(6, -1, -1)] 
+    quantities = list()
+
+    for date in dates:
+        sales_quantity = Outflow.objects.filter(created_at__date=date).count()
+        quantities.append(sales_quantity)
+
+    return dict(
+        dates=dates,
+        values=quantities,
+    )
+
+
+def get_graphic_product_category_metric():
+    categories = Category.objects.all()
+    return {category.name : Product.objects.filter(category=category).count() for category in categories} # Percorre as categorias e retorna a quantidade de produtos por categoria
+
+
+def get_graphic_product_brand_metric():
+    brands = Brand.objects.all()
+    return {brand.name : Product.objects.filter(brand=brand).count() for brand in brands}
+
+
+
 
